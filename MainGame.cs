dc7400ad3bfdc94466fee5b4cc2c508d0bc5e8e4
Meteoroetem <114942884+Meteoroetem@users.noreply.github.com>
2023-04-14 21:08:23 +0300

    Finished the animation engine

--- MainGame.cs
+++ MainGame.cs
@@ -9,8 +9,11 @@ public class MainGame : Game
 	private GraphicsDeviceManager _graphics;
 	private SpriteBatch spriteBatch;
     Player myPlayer;
+    bool isKeyJustPressed = false;
+    bool wasKeyPressed = false;
 
-	public MainGame()
+
+    public MainGame()
 	{
 		_graphics = new GraphicsDeviceManager(this);
 		Content.RootDirectory = "Content";
@@ -29,17 +32,41 @@ public class MainGame : Game
         // TODO: use this.Content to load your game content here
         myPlayer = new(
             Content.Load<Texture2D>("Short_Sunflower_Sprite_Sheet"),
-            new Rectangle[2]{new(0,0,11,20), new(0,22,11,19)}, new Rectangle[0], new Rectangle[0]);
+            new Rectangle[2]{
+                new(0,0,11,20), new(0,21,11,20)}, 
+            new Rectangle[2]{
+                new(12,0,11,20), new(12,21,11,20)
+            }, 
+            new Rectangle[2]{
+                new(24,0,11,20), new(24,21,11,20)
+            });
 	}
 
 	protected override void Update(GameTime gameTime)
 	{
 		if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 			Exit();
+        
+        // TODO: Add your update logic here
+        if(Keyboard.GetState().IsKeyDown(Keys.Right)){
+            isKeyJustPressed = wasKeyPressed ? false : true;
+            myPlayer.CurrentAnimation = "Right";
+            if(Keyboard.GetState().IsKeyDown(Keys.Left)){
+                myPlayer.CurrentAnimation = "Idle";
+            }
+        }
+        else if(Keyboard.GetState().IsKeyDown(Keys.Left)){
+            isKeyJustPressed = wasKeyPressed ? false : true;
+            myPlayer.CurrentAnimation = "Left";
+        }
+        else{
+            myPlayer.CurrentAnimation = "Idle";
+        }
+        wasKeyPressed = !Keyboard.GetState().IsKeyUp(Keys.Right) || !Keyboard.GetState().IsKeyUp(Keys.Left);
 
-		// TODO: Add your update logic here
-        myPlayer.NextFrame(gameTime.TotalGameTime.TotalMilliseconds);
-		base.Update(gameTime);
+        myPlayer.FrameTimer += gameTime.ElapsedGameTime.TotalSeconds;
+        myPlayer.NextFrame();
+        base.Update(gameTime);
 	}
 
 	protected override void Draw(GameTime gameTime)
@@ -48,8 +75,8 @@ public class MainGame : Game
 
 		// TODO: Add your drawing code here
         spriteBatch.Begin(samplerState:SamplerState.PointClamp);
-        spriteBatch.Draw(myPlayer.spriteSheet, myPlayer.GetArea(), 
-            myPlayer.idleFrames[0], Color.White);
+        spriteBatch.Draw(myPlayer.spriteSheet, myPlayer.Area, 
+            myPlayer.CurrentSprite, Color.White);
         spriteBatch.End();
         base.Draw(gameTime);
 	}
--- Player.cs
+++ Player.cs
@@ -6,42 +6,89 @@ namespace Monogame_Test;
 
 class Player
 {
+    /// <summary>
+    /// Determines how many frames per second the animation will have
+    /// </summary>
+    /// <value>The fps of the animation</value>
+    public float animationSpeed = 3;
+    public int spriteScale = 10;
     public Player(Texture2D _spriteSheet, Rectangle[] _idleFrames, Rectangle[] _rightFrames, Rectangle[] _leftFrames){
         spriteSheet = _spriteSheet;
         idleFrames = _idleFrames;
         rightFrames = _rightFrames;
         leftFrames = _leftFrames;
-        area = new Rectangle(0,0,220,400);
+        _area = new Rectangle(0,0,11*spriteScale,20*spriteScale);
     }
 	public readonly Texture2D spriteSheet;
 	public readonly Rectangle[] idleFrames;
 	public readonly Rectangle[] rightFrames;
 	public readonly Rectangle[] leftFrames;
-    private Rectangle area;
-    public Rectangle GetArea() => area;
-    private Rectangle currentSprite;
-    public Rectangle GetCurrentSprite() => currentSprite;
-    private string currentAnimation = "Idle";
-    private int currentFrame = 0;
-
-	public void NextFrame(double elapsedMillis){
-        if(elapsedMillis % 30 == 0){
-            currentFrame = currentFrame > 0 ? 0 : 1;
-            try{
-                switch (currentAnimation){
-                    case "Idle":
-                        currentSprite = idleFrames[currentFrame];
-                        area.Height = currentSprite.Height*20;
-                        break;
-                    case "Right":
-                        currentSprite = rightFrames[currentFrame];
-                        break;
-                    case "Left":
-                    currentSprite = leftFrames[currentFrame];
-                    break;
+    private string _currentAnimation = "Idle";
+    /// <summary>
+    /// Gets the a string representing the current animation.
+    /// Setter only allows "Idle", "Right" or "Left" as values, any other value will be ignored.
+    /// After setting, the animation will immediately change with the current frame as the starting point.
+    /// </summary>
+    /// <value>A string that represents the current animation</value>
+    public string CurrentAnimation{
+        get => _currentAnimation;
+        set{
+            if(value == "Idle" || value == "Right" || value == "Left"){
+                if (_currentAnimation != value)
+                {
+                    _currentAnimation = value;
+                    var currentFrames = (Rectangle[])this.GetType().GetField(value.ToLower() + "Frames").GetValue(this);
+                    _currentSprite = currentFrames[_currentFrame];
                 }
             }
-            catch(IndexOutOfRangeException){}
+        }
+    }
+    private int _currentFrame = 0;
+    private bool _nextFrame = false;
+    /// <value>
+    /// The sprite's scale
+    /// </value>
+    private Rectangle _area;
+    /// <summary>
+    /// A rectangle that represents the location and boundries of the player
+    /// </summary>
+    /// <value>The location and boundries of the player</value>
+    public Rectangle Area => _area;
+
+    private Rectangle _currentSprite;
+    /// <summary>
+    /// The rectangle of the current sprite in the spritesheet
+    /// </summary>
+    /// <value>The location of the current sprite of the player on the spritesheet</value>
+    public Rectangle CurrentSprite => _currentSprite;
+
+    private double _frameTimer = 0;
+    /// <summary>
+    /// Counts how many seconds have passed since the last frame change.
+    /// Changes in relation to the animation speed.
+    /// </summary>
+    /// <value>The seconds that have passed since the last frame</value>
+    public double FrameTimer {
+        get => _frameTimer;
+        set{
+            if(value >= 1/animationSpeed){
+                _nextFrame = true;
+                _frameTimer = 0;
+            }
+            else{
+                _frameTimer = value;
+            }
+        }
+    }
+
+	public void NextFrame(){
+        if(_nextFrame){
+            _currentFrame = 1 - _currentFrame;
+            var _currentFrames =
+                (Rectangle[])
+                this.GetType().GetField(_currentAnimation.ToLower() + "Frames").GetValue(this);
+            _currentSprite = _currentFrames[_currentFrame];
+            _nextFrame = false;
         }
     }
 }
